00aa99a4e86685861a2f2b9ff75df28ec345fc4b
Mina Nabil <mina9492@hotmail.com>
2024-10-15 13:22:26 +0300

    adjusting seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,16 +13,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::newUser("mark", "Mark", "Mourad", User::TYPE_ADMIN, "mark@genuine");
         User::newUser("michael", "Michael", "Rafaillo", User::TYPE_ADMIN, "michael@genuine");
         User::newUser("mina", "Mina", "Nabil", User::TYPE_ADMIN, "mina@genuine");
-        for ($i = 51; $i <= 100; $i++) {
-            User::newUser(
-                "user{$i}",                       // Unique username
-                "FirstName{$i}",                   // First name
-                "LastName{$i}",                    // Last name
-                User::TYPE_ADMIN,                  // Assuming all users are admin, change as needed
-                "user{$i}@genuine.com"             // Unique email
-            );
-        }
+        // for ($i = 51; $i <= 100; $i++) {
+        //     User::newUser(
+        //         "user{$i}",                       // Unique username
+        //         "FirstName{$i}",                   // First name
+        //         "LastName{$i}",                    // Last name
+        //         User::TYPE_ADMIN,                  // Assuming all users are admin, change as needed
+        //         "user{$i}@genuine.com"             // Unique email
+        //     );
+        // }
     }
 }
