d21c5b94104c93592d53f5e163ae2d69dd6683fa
Mina Nabil <mina9492@hotmail.com>
2025-01-02 15:24:38 +0200

    fixing report

--- resources/views/livewire/orders/order-index.blade.php
+++ resources/views/livewire/orders/order-index.blade.php
@@ -439,7 +439,7 @@
                                 </td>
 
                                 <td class="table-td text-start overflow-hidden text-ellipsis whitespace-nowrap">
-                                    {{ $order->driver ? $order->driver->shift_title : '-' }} {{ ($order->driver && $order->driver_payment_type) ? ' • ' . $order->driver_payment_type : ''}}
+                                    {{ $order->driver ? $order->driver->shift_title : '-' }} {{ ($order->driver && $order->driver_payment_type) ? ' • ' . paymentMethodInArabic($order->driver_payment_type) : ''}}
                                 </td>
 
                                 <td class="table-td text-start overflow-hidden text-ellipsis whitespace-nowrap">
