057e15ed3dcc6430a1a96153f915ff416b6bca14
Mina Nabil <mina9492@hotmail.com>
2025-01-02 14:23:15 +0200

    fixing report

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -260,7 +260,8 @@
                             </span>
                         </a>
                     </li>
-                    <li>
+
+                    {{-- <li>
                         <a href="{{ url('/report/customers') }}" class="navItem {{ $customerReport ?? '' }}">
                             <span class="flex
                             items-center">
@@ -269,8 +270,7 @@
                                 <span>تقرير عملاء</span>
                             </span>
                         </a>
-                    </li>
-
+                    </li> --}}
 
                     <li class="sidebar-menu-title">Database</li>
 
