e77fedacc91f53b9d1ec2d639d170bf6adc9cdbe
Michael Rafaillo <michael.rafaillo@gmail.com>
2025-01-02 11:09:04 +0200

    fix order location url

--- resources/views/livewire/orders/order-show.blade.php
+++ resources/views/livewire/orders/order-show.blade.php
@@ -572,6 +572,7 @@
                                     @endif
                                 </p>
                             </div>
+
                             <div class="flex justify-bewwteen mt-5">
                                 <label for="phone" class="form-label"><b>Shipping Address</b></label>
                                 <button wire:click='openUpdateShippingDetails' class="action-btn" type="button">
@@ -583,6 +584,20 @@
                                 href="tel:{{ $order->customer_phone }}">{{ $order->customer_phone }}</a>
                             <p class="text-xs">{{ $order->shipping_address }}</p>
                             <p class="text-xs mt-1">{{ $order->zone->name }}</p>
+
+                            <div class="flex justify-bewwteen mt-5">
+                                <label for="phone" class="form-label"><b>Location URL</b></label>
+                                <button wire:click='openUpdateLocationUrl' class="action-btn" type="button">
+                                    <iconify-icon icon="heroicons:pencil-square"></iconify-icon>
+                                </button>
+                            </div>
+                            @if ($order->location_url)
+                                <p class="text-xs clickable-link"><a target="_blanck" href="{{ $order->location_url }}">{{ $order->location_url }}</a></p>
+                            @else
+                                <p class="text-xs">N/A</p>
+                            @endif
+
+
                         </div>
                     </div>
                 </div>
@@ -874,6 +889,68 @@
                                     <span wire:loading.remove wire:target="updateShippingDetails">Submit</span>
                                     <iconify-icon class="text-xl spin-slow ltr:mr-2 rtl:ml-2 relative top-[1px]"
                                         wire:loading wire:target="updateShippingDetails"
+                                        icon="line-md:loading-twotone-loop"></iconify-icon>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        @endif
+    @endcan
+
+    @can('update', $order)
+        @if ($updateLocationSec)
+            <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto show"
+                tabindex="-1" aria-labelledby="vertically_center" aria-modal="true" role="dialog"
+                style="display: block;">
+                <div class="modal-dialog relative w-auto pointer-events-none">
+                    <div
+                        class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                        <div class="relative bg-white rounded-lg shadow dark:bg-slate-700">
+                            <!-- Modal header -->
+                            <div
+                                class="flex items-center justify-between p-5 border-b rounded-t dark:border-slate-600 bg-black-500">
+                                <h3 class="text-xl font-medium text-white dark:text-white capitalize">
+                                    Edit location url
+                                </h3>
+                                <button wire:click="closeUpdateLocationUrl" type="button"
+                                    class="text-slate-400 bg-transparent hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-slate-600 dark:hover:text-white"
+                                    data-bs-dismiss="modal">
+                                    <svg aria-hidden="true" class="w-5 h-5" fill="#ffffff" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+
+                            <!-- Modal body -->
+                            <div class="p-6 space-y-4">
+
+                                <div class="from-group">
+                                    <div class="input-area">
+                                        <label for="location_url" class="form-label">Location URL*</label>
+                                        <textarea id="location_url" type="text"
+                                            class="form-control @error('location_url') !border-danger-500 @enderror" wire:model="location_url"
+                                            autocomplete="off"></textarea>
+                                    </div>
+                                    @error('location_url')
+                                        <span
+                                            class="font-Inter text-sm text-danger-500 pt-2 inline-block">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                            </div>
+
+                            <!-- Modal footer -->
+                            <div class="flex items-center justify-end p-6 border-t border-slate-200 rounded-b">
+                                <button wire:click="updateLocationUrl" data-bs-dismiss="modal"
+                                    class="btn inline-flex justify-center text-white bg-black-500">
+                                    <span wire:loading.remove wire:target="updateLocationUrl">Submit</span>
+                                    <iconify-icon class="text-xl spin-slow ltr:mr-2 rtl:ml-2 relative top-[1px]"
+                                        wire:loading wire:target="updateLocationUrl"
                                         icon="line-md:loading-twotone-loop"></iconify-icon>
                                 </button>
                             </div>
@@ -1393,7 +1470,8 @@
                                             <option value="">Return to customer balance</option>
                                             @foreach ($PAYMENT_METHODS as $PAYMENT_METHOD)
                                                 <option value="{{ $PAYMENT_METHOD }}">
-                                                    Returned {{ ucwords(str_replace('_', ' ', $PAYMENT_METHOD)) }}</option>
+                                                    Returned {{ ucwords(str_replace('_', ' ', $PAYMENT_METHOD)) }}
+                                                </option>
                                             @endforeach
 
                                         </select>
@@ -1676,7 +1754,7 @@
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd"
                                             d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10
-                                                                        11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                                            11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                                             clip-rule="evenodd"></path>
                                     </svg>
                                     <span class="sr-only">Close modal</span>
