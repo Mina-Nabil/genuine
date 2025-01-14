74069a42efe9534587668977874b0b0a8e946bbe
Michael Rafaillo <michael.rafaillo@gmail.com>
2025-01-14 12:18:43 +0200

    adding migrations

---
+++ database/migrations/2025_01_11_134320_create_supplier_invoices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplier_invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supplier_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 15, 2);
+            $table->date('payment_due')->nullable();
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplier_invoices');
+    }
+};
---
+++ database/migrations/2025_01_11_135552_update_customer_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customer_payments', function (Blueprint $table) {
+            $table->foreignId('invoice_id')->nullable()->after('order_id')->constrained('supplier_invoices')->onDelete('cascade');
+            $table->foreignId('customer_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customer_payments', function (Blueprint $table) {
+            $table->dropForeign(['invoice_id']);
+            $table->dropColumn('invoice_id');
+            $table->foreignId('customer_id')->nullable(false)->change();
+        });
+    }
+};
