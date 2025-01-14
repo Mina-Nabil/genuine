6e6fbbd4923c338eff782605a340d1f731996387
Mina Nabil <mina9492@hotmail.com>
2025-01-14 12:01:55 +0200

    adding discount and delivery edits

--- app/Models/Orders/Order.php
+++ app/Models/Orders/Order.php
@@ -1904,7 +1904,7 @@ class Order extends Model
                     WHEN DAY(orders.delivery_date) BETWEEN 1 AND 7 THEN 1
                     WHEN DAY(orders.delivery_date) BETWEEN 8 AND 14 THEN 2
                     WHEN DAY(orders.delivery_date) BETWEEN 15 AND 21 THEN 3
-                    ELSE 4 END AS as week')
+                    ELSE 4 END AS week')
             ->selectRaw('COUNT(orders.id) as total_orders')
             ->join('zones', 'zones.id', '=', 'orders.zone_id')
             ->whereYear('orders.delivery_date', $year)
