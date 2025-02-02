7cb079cc88f99ca5399eec22d5f592791499412e
Mina Nabil <mina9492@hotmail.com>
2025-02-02 11:55:13 +0200

    merged

--- app/Livewire/Materials/InvoiceCreate.php
+++ app/Livewire/Materials/InvoiceCreate.php
@@ -125,7 +125,7 @@ class InvoiceCreate extends Component
 
         $this->subtotal = $subtotal;
         $this->totalItems = $totalItems;
-        $this->total = $subtotal  + $extraFee;
+        $this->total = $subtotal  + is_numeric($extraFee) ? $extraFee : 0;
     }
 
     public function selectSupplier($id)
