b71c479b6776131b3076c4e6b23ce360fe1762e2
Mina Nabil <mina9492@hotmail.com>
2025-02-05 13:17:36 +0200

    finalizaing

--- app/Models/Orders/Order.php
+++ app/Models/Orders/Order.php
@@ -492,9 +492,9 @@ class Order extends Model
             ->selectRaw('SUM((SELECT SUM(total_amount) from orders as o2 where 
             o1.id = o2.id and o2.is_debit = 1 
             and o2.deleted_at is null 
-            and o2.is_confirmed=1
+            and o2.is_confirmed = 1
             and o2.delivery_date between "' . $startDay->format('Y-m-d 00:00:00') . '" and "' . $endDay->format('Y-m-d 23:59:59')  . '"
-            and `o2`.`status` in (' . implode(", ", Order::OK_STATUSES) . ')
+            and `o2`.`status` in ("new", "ready", "in_delivery", "done")
             )) as total_debit ')
             ->leftjoin('drivers', 'drivers.id', '=', 'o1.driver_id')
             ->leftjoin('users', 'users.id', '=', 'drivers.user_id')
