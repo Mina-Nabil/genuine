45a500eba35ee11cff7b3b2a362352c83ae99709
Mina Nabil <mina9492@hotmail.com>
2025-02-06 11:59:18 +0200

    finalizaing

--- app/Models/Orders/Order.php
+++ app/Models/Orders/Order.php
@@ -2189,7 +2189,7 @@ class Order extends Model
 
     public static function getTotalDebit()
     {
-        return self::selectRaw('SUM(amount) as total_debit')
+        return self::selectRaw('SUM(total_amount) as total_debit')
             ->where('is_debit')->whereNot('is_paid')
             ->get()->first()?->total_debit;
     }
