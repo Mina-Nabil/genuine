959a42f528faf165f11fd202bcbc5bf7e0df9886
Mina Nabil <mina9492@hotmail.com>
2025-03-12 11:03:53 +0200

    fixing order close

--- app/Models/Orders/Order.php
+++ app/Models/Orders/Order.php
@@ -996,7 +996,7 @@ class Order extends Model
                 if ($this->remaining_to_pay == 0) {
                     $this->is_paid = true;
                     $this->save();
-                    if (($this->is_confirmed && $this->is_ready) || $this->is_in_delivery || $this->is_delivered) {
+                    if ($this->is_in_delivery && $this->is_delivered) {
                         $this->is_delivered = true;
                         $this->save();
                         $this->setStatus(self::STATUS_DONE, true);
