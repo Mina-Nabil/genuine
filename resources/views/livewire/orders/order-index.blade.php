9ba9954a5bf1a1ceef554e6bef6f3643b8d8048d
Mina Nabil <mina9492@hotmail.com>
2025-03-15 14:32:46 +0200

    debugging errors

--- resources/views/livewire/orders/order-index.blade.php
+++ resources/views/livewire/orders/order-index.blade.php
@@ -333,14 +333,14 @@
 
                                 <td class="table-td">
                                     @if (
-                                        ($order->delivery_date->isPast() && !$order->delivery_date->isToday()) ||
-                                            ($order->delivery_date->isToday() && !$order->is_confirmed))
+                                        ($order->delivery_date?->isPast() && !$order->delivery_date?->isToday()) ||
+                                            ($order->delivery_date?->isToday() && !$order->is_confirmed))
                                         <span
                                             class="h-[6px] w-[6px] bg-danger-500 rounded-full inline-block ring-4 ring-opacity-30 ring-danger-500"
                                             style="vertical-align: middle;"></span> &nbsp;
                                     @endif
 
-                                    {{ $order->delivery_date->isToday() ? 'Today' : ($order->delivery_date->isYesterday() ? 'Yesterday' : $order->delivery_date->format('D d-m')) }}
+                                    {{ $order->delivery_date?->isToday() ? 'Today' : ($order->delivery_date?->isYesterday() ? 'Yesterday' : $order->delivery_date?->format('D d-m')) }}
                                 </td>
 
                                 <td class="table-td">
